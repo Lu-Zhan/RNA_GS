b6a7bf682ddd7b95fcfb38c9907c02696e9ed573
LuZhan <zhannixl@gmail.com>
2024-01-17 02:10:30 +0800

    fix scripts

--- scripts/lz.sh
+++ scripts/lz.sh
@@ -2,10 +2,10 @@ gpu=3
 
 
 CUDA_VISIBLE_DEVICES=$gpu python train.py \
-    --weights 0 1 0 0 0 0 0.001 0.1 \
+    --weights 0 1 0 0 0 0 0.001 0.001 \
     --iterations 20000 \
     --lr 0.002 \
-    --exp_name codecos_0.001 \
+    --exp_name codecos_0.001_scale_0.001 \
     --img_path ../data/1213_demo_data_v2/raw1 \
     --codebook_path ../data/1213_demo_data_v2/codebook.xlsx \
 
