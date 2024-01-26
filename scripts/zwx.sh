95febffbfc32e3db6c4e57576da9524dd5ba3bbf
zwx <599284742@qq.com>
2024-01-26 09:28:38 +0800

    update

--- scripts/zwx.sh
+++ scripts/zwx.sh
@@ -13,24 +13,24 @@ CUDA_VISIBLE_DEVICES=$gpu python train.py \
     --dens_flags 0 0 0\
     --pos_score 1 \
 
-# CUDA_VISIBLE_DEVICES=$gpu python train.py \
-#     --weights 0 1 0 0 0 0 0 0 0 0 0 0.01\
-#     --iterations 20000 \
-#     --lr 0.002 \
-#     --exp_name ablation_mip_0.01 \
-#     --img_path ./data/1213_demo_data_v2/raw1 \
-#     --codebook_path ./data/codebook.xlsx \
-#     --cali_loss_type "cos" \
-#     --dens_flags 0 0 0\
-#     --pos_score 1 \
+CUDA_VISIBLE_DEVICES=$gpu python train.py \
+    --weights 0 1 0 0 0 0 0 0 0 0 0 0.01\
+    --iterations 20000 \
+    --lr 0.002 \
+    --exp_name ablation_mip_0.01 \
+    --img_path ./data/1213_demo_data_v2/raw1 \
+    --codebook_path ./data/codebook.xlsx \
+    --cali_loss_type "cos" \
+    --dens_flags 0 0 0\
+    --pos_score 1 \
 
-# CUDA_VISIBLE_DEVICES=$gpu python train.py \
-#     --weights 0 1 0 0 0 0 0 0 0 0 0 0.001\
-#     --iterations 20000 \
-#     --lr 0.002 \
-#     --exp_name ablation_mip_0.001 \
-#     --img_path ./data/1213_demo_data_v2/raw1 \
-#     --codebook_path ./data/codebook.xlsx \
-#     --cali_loss_type "cos" \
-#     --dens_flags 0 0 0\
-#     --pos_score 1 \
+CUDA_VISIBLE_DEVICES=$gpu python train.py \
+    --weights 0 1 0 0 0 0 0 0 0 0 0 0.001\
+    --iterations 20000 \
+    --lr 0.002 \
+    --exp_name ablation_mip_0.001 \
+    --img_path ./data/1213_demo_data_v2/raw1 \
+    --codebook_path ./data/codebook.xlsx \
+    --cali_loss_type "cos" \
+    --dens_flags 0 0 0\
+    --pos_score 1 \
